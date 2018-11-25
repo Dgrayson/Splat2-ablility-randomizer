71bc061ecfec7e7e53f3de154dbd0bda8c16692f
Darayavaus Grayson <dgrayson100@gmail.com>
2018-11-24 22:30:50 -0500

    Updated script to display randomized icons

--- script.js
+++ script.js
@@ -3,31 +3,16 @@ var abilites = ["BombResistance", "ColdBlooded", "Comeback", "Doubler", "DropRol
 				"ObjectShredder", "OpeningGambit", "QuickJump", "QuickRespawn", "RespawnPunisher", "RunSpeedUp", "SpecialChargeUp", "SpecialPowerUp", "SpecialSaver", "StealthJump", "SubPowerUp", "SwimSpeedUp", 
 				"Tenacity", "ThermalInk"]; 
 
-$("#randomize-btn").click(RandomizeIcon()); 
 
-$(document).ready(function(){
-	console.log("ready!"); 
-});
 
-function getIcon(num, iconNum){
+$(document).ready(main);
 
-	var icon = abilites[num]; 
-	var iconPath = "/images/Icons/" + icon + ".png"; 
-	console.log(iconPath);
+function main(){
 
-	if(iconNum === 1){
-		$('#icon1').attr('src', iconPath); 
-	}
-	else if(iconNum === 2){
-		$('#icon2').attr('src', iconPath); 
-	}
-	else if(iconNum === 3){
-		$('#icon3').attr('src', iconPath); 
-	}
+	$("#randomize-btn").click(randomizeIcon); 
 }
 
-function RandomizeIcon(){
-	
+function randomizeIcon(){
 	var num1 = Math.floor(Math.random() * 25); 
 	var num2 = Math.floor(Math.random() * 25); 
 	var num3 = Math.floor(Math.random() * 25); 
@@ -35,5 +20,27 @@ function RandomizeIcon(){
 	getIcon(num1, 1); 
 	getIcon(num2, 2); 
 	getIcon(num3, 3); 
+}
+function getIcon(num, iconNum){
+
+	var icon = abilites[num]; 
+	var iconPath = "images/icons/" + icon + ".png"; 
+	console.log(iconPath);
+
+	if(iconNum == 1){
+		$('#icon1').attr('src', iconPath); 
+
+	}
+	else if(iconNum == 2){
+		$('#icon2').attr('src', iconPath); 
+
+	}
+	else if(iconNum == 3){
+		$('#icon3').attr('src', iconPath); 
+
+	}
+	else{
+		console.log("image not changed");
+	}
 }
 
